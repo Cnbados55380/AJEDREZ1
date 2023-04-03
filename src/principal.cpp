1d604d521b9c82122a3b5cad078a6ba8a580c0ff
Cnbados55380 <61271634+Cnbados55380@users.noreply.github.com>
2023-04-03 19:52:11 +0200

    Create principal.cpp
    Es la pantalla principal, donde sale una pantalla inicial para iniciar el juego dandole a la tecla (I o i) y cuando este detecta que se ha pulsado pasa al tablero de juego

---
+++ src/principal.cpp
@@ -0,0 +1,44 @@
+#include "ETSIDI.h"
+#include "freeglut.h"
+
+//los callback, funciones que seran llamadas automaticamente por la glut
+//cuando sucedan eventos
+//NO HACE FALTA LLAMARLAS EXPLICITAMENTE
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
+
+ETSIDI::Sprite pantallaInicio("C:/Users/Cristina/source/repos/pruebajuego2/imagen/start.png", 0, 0, 8000, 6000);
+ETSIDI::Sprite tablero("C:/Users/Cristina/source/repos/pruebajuego2/imagen/Tablero.png", 0, 0, 8000, 6000);
+
+bool juegoIniciado = false;
+
+int main(int argc,char* argv[])
+{
+	//Inicializar el gestor de ventanas GLUT
+	//y crear la ventana
+	glutInit(&argc, argv);
+	glutInitWindowSize(800,600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("Ajedrez");
+
+	//habilitar luces y definir perspectiva
+	/*glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);	
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective( 40.0, 800/600.0f, 0.1, 150);
+	*/
+	//Registrar los callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(25,OnTimer,0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
+	glutKeyboardFunc(OnKeyboardDown);
+
+	//mundo.inicializa();
+		
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();	
+
+	return 0;   
+}
