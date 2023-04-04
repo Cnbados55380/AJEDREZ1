92b294b40ebbf5457a006be1a39d26165e4e7011
Cnbados55380 <61271634+Cnbados55380@users.noreply.github.com>
2023-04-04 18:09:36 +0200

    Add files via upload

---
+++ lib/tablero.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+class tablero
+{
+private:
+
+	char casilla[8][8]; //matriz 8x8 para el tablero
+	
+	//static const std::string nombresPiezas[8];
+	//Pieza* piezas[32];
+
+public:
+
+	tablero();
+	~tablero();
+
+	void dibuja();
+};
